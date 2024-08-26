a8143a51fcca8c815dbb2d8c2d841a1f0b3adde3
sirajulhassan <thayyilsiraj911@gmail.com>
2024-08-26 11:13:32 +0530

    test

--- src/main/java/com/learning/springBoot/game/AppGamingBasic.java
+++ src/main/java/com/learning/springBoot/game/AppGamingBasic.java
@@ -1,14 +1,11 @@
 package com.learning.springBoot.game;
 
 public class AppGamingBasic {
-
 	public static void main(String[] args) {
 		
 		var mario = new MarioGame();
 		var contra = new ContraGame();
 		var pac = new PacManGame();
-		
 		var gameRunner = new GameRunner(pac);
 	}
-
 }
