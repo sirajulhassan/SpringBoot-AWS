e8ba0b9c92f06541bf37abf4ea55b419f44fbf68
sirajulhassan <thayyilsiraj911@gmail.com>
2024-08-26 12:32:59 +0530

    making the context class to config and ddefining beans inside it

--- src/main/java/com/learning/springBoot/game/springGame/AppGamingBasicSpring.java
+++ src/main/java/com/learning/springBoot/game/springGame/AppGamingBasicSpring.java
@@ -1,15 +1,32 @@
 package com.learning.springBoot.game.springGame;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.learning.springBoot.game.GameConsole;
 import com.learning.springBoot.game.GameRunner;
+import com.learning.springBoot.game.PacManGame;
 
-
+@Configuration
 public class AppGamingBasicSpring {
+	
+	@Bean
+	public GameConsole game() {
+		var game = new PacManGame();
+		return game;
+		
+	}
+	@Bean
+	GameRunner gameRunner(GameConsole game) {
+		var gameRunner = new GameRunner(game);
+		return gameRunner;
+		
+	}
+	
 	public static void main(String[] args) {
 		
-		try(var context =new AnnotationConfigApplicationContext(AppGamingBasicSpringConfig.class)){
+		try(var context =new AnnotationConfigApplicationContext(AppGamingBasicSpring.class)){
 			context.getBean(GameRunner.class);
 		}
 		
