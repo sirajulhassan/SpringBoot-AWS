71d32ab0c12f8505c4b244c0c1fe679754080491
sirajulhassan <thayyilsiraj911@gmail.com>
2024-08-28 22:45:05 +0530

    dependency injection

--- src/main/java/com/learning/springBoot/game/AGamingAppLauncherDependencyInjection.java
+++ src/main/java/com/learning/springBoot/game/AGamingAppLauncherDependencyInjection.java
@@ -2,9 +2,100 @@ package com.learning.springBoot.game;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+
+
+/*
+ * ++Dependency Injection start++
+ * 
+ * @AUTOWIRED
+ * 1. Field based Dependency Injection
+ * 2. constructor based Dependency Injection
+ * 3. setter based  Dependency Injection
+
+*/
+
+@Component
+class Dependency1{
+	
+}
+@Component
+class Dependency2{
+	
+}
+@Component
+class BussinessClass{
+	
+	//1. field-based
+	//@Autowired
+	Dependency1 dep1;
+	//@Autowired
+	Dependency2 dep2;
+	
+	/*
+	//2. Setter-based
+	@Autowired
+	public void setDep1(Dependency1 dep1) {
+		this.dep1 = dep1;
+	}
+
+	@Autowired
+	public void setDep2(Dependency2 dep2) {
+		this.dep2 = dep2;
+	}
+	*/
+	
+	// 3. constructor based (most preferred )
+	// @Autowired -in contructor based , no need to specify autowired
+	public BussinessClass(Dependency1 dep1, Dependency2 dep2) {
+		super();
+		this.dep1 = dep1;
+		this.dep2 = dep2;
+	}
+	
+	
+	
+	
+	@Override
+	public String toString() {
+		return "BussinessClass [dep1=" + dep1 + ", dep2=" + dep2 + "]";
+	}
+
+	
+
+
+	
+	
+	
+	
+	
+	
+}
+
+/*
+ * Dependency Injection end
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 @Configuration
@@ -18,7 +109,7 @@ public class AGamingAppLauncherDependencyInjection {
 			System.out.println("Here");
 			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
 		
-		
+			System.out.println(context.getBean(BussinessClass.class).toString());
 		
 		
 		}
