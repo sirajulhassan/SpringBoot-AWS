eea07d5db50fe5efa9a2f2b3b9fbfbd4e81cc7e5
sirajulhassan <thayyilsiraj911@gmail.com>
2024-09-04 17:13:08 +0530

    applicationConfig,Actuator,Profile, ExternalConfiguration using appconfig

--- src/main/java/com/learning/springBoot/LeraningController.java
+++ src/main/java/com/learning/springBoot/LeraningController.java
@@ -3,15 +3,27 @@ package com.learning.springBoot;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LeraningController {
+	@Autowired
+	CurrencyServiceConfigurationExample currencyServiceConfigurationExample;
+	
 	@RequestMapping("/courses")
 	public List<Course> getCourses(){
+		
+		//testing externalizing configuration
+		System.out.println(currencyServiceConfigurationExample.getUrl());
+		System.out.println(currencyServiceConfigurationExample.getUser());
+		System.out.println(currencyServiceConfigurationExample.getKey());
+		
 		return Arrays.asList(new Course(1,"Python"),
-				new Course(1,"Java"));
+				new Course(2,"Java"),
+				new Course(3,"C#"),
+				new Course(4,"MySQL"));
 		
 	}
 	
