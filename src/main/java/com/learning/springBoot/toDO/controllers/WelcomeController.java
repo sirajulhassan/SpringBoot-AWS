a02ea07eb5f735bf820431efa1965fcec1c41413
sirajulhassan <thayyilsiraj911@gmail.com>
2024-09-16 11:46:06 +0530

    spring security - initial refactoring no need of the login logic, login.jsp and login.controller now.

---
+++ src/main/java/com/learning/springBoot/toDO/controllers/WelcomeController.java
@@ -0,0 +1,51 @@
+package com.learning.springBoot.toDO.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.learning.springBoot.toDO.services.LoginService;
+
+
+
+@Controller
+@SessionAttributes("name")
+public class WelcomeController {
+	
+	@RequestMapping(value ="/", method = RequestMethod.GET)
+	public String gotoWelcomePage(ModelMap model) {
+		model.put("name", "Benazin");
+		return "welcome";
+	}
+	
+	
+	//going to use spring security - no need of the login logic, login.jsp and login.controller
+	
+	/*
+	 * 
+	 * LoginService loginService;
+	 * 
+	 * 
+	 * 
+	 * public LoginController(LoginService loginService) { super();
+	 * this.loginService = loginService; System.out.println("login controller"); }
+	 * 
+	 * @RequestMapping(value ="login", method = RequestMethod.GET) public String
+	 * login() { return "login"; }
+	 * 
+	 * //for the form from login(post)
+	 * 
+	 * @RequestMapping(value ="login", method = RequestMethod.POST) public String
+	 * auth(@RequestParam String name,@RequestParam String password, ModelMap model
+	 * ) {
+	 * 
+	 * if(loginService.validateUser(name, password)) { model.put("name", name);
+	 * return "welcome"; } else { model.put("errorMsg", "invalid user"); return
+	 * "login"; } }
+	 * 
+	 * 
+	 */
+}
