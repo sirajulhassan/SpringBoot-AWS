a6a52cff3011ae828392f56054e10fb2f3902046
sirajulhassan <thayyilsiraj911@gmail.com>
2024-09-20 12:10:35 +0530

    s13.V154 updating the spring securityFilterChain to enable H2 console
    -updating SecurityfilterChain for H2 console page loading error(it is
    using html frames and spring security is by default disable it)
    -to do that we need to override filter chain and manuelly add the
    default chains aswell and code to disable csrf and frames
    
    --configure below things in custom filter
    	1.default chain -all URLs are protected
    	2.default chain adding default chain - redirect to default login page
    for un autorized requests.
    
    	3.cusomize - disable csrf
    	4.customize- enable frames

--- src/main/java/com/learning/springBoot/security/SpringSecurityConfiguration.java
+++ src/main/java/com/learning/springBoot/security/SpringSecurityConfiguration.java
@@ -1,15 +1,18 @@
 package com.learning.springBoot.security;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import java.util.function.Function;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
+import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SpringSecurityConfiguration {
 //	normally we use LDAP or DATAbase
@@ -31,8 +34,41 @@ public class SpringSecurityConfiguration {
 	}
 	
 	// custom password encorder using Bcrypt
-			@Bean
-			public PasswordEncoder passwordEncoder() {
-				return new BCryptPasswordEncoder();
-			}
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	
+	
+	//updating SecurityfilterChain for H2 console page loading error(it is using html frames and spring security is by default disable it)
+	//to do that we need to override filter chain and manuelly add the default chains and disable csrf and frames
+	
+	//configure below things in custom filter
+	// 1.default chain -all URLs are protected
+	// 2.default chain adding default chain - redirect to default login page for un autorized requests.
+	
+	//3.cusomize - disable csrf
+	//4.customize- enable frames
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http ) throws Exception{
+		// adding default chain - to protect all requests
+		http.authorizeHttpRequests(auth->auth.anyRequest().authenticated());
+		
+		// adding default chain - redirect to default login page for un autorized requests
+		http.formLogin(withDefaults());
+		
+		//disable csrf
+		http.csrf().disable();
+		
+		//disable blocking frames
+		http.headers().frameOptions().disable();
+		
+		return http.build();
+	}
+	
+	
+	
+	
+	
 }
