0e6b153ab2cdc313905ad45c0201ba18fd8cf318
sirajulhassan <thayyilsiraj911@gmail.com>
2024-09-21 12:25:19 +0530

    s13.v155 adding data using data.sql
    1. sql file get executed before the entities are getting created. which
    will lead to an error while inserting data .to ialize the entity first
    add below config in app.prop
    spring.jpa.defer-datasource-initialization=true
    
    2.added insert queries in data.sql
    data.sql and schema.sql automatically will get executed once app start.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,3 @@
-create table course(id bigint not null, name varchar(50) not null,author varchar(50) ,primary key(id));
+--create table course(id bigint not null, name varchar(50) not null,author varchar(50) ,primary key(id));
 
 insert into course(id,name,author) values(1, 'testCourse1', 'testAuthor1');
